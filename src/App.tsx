80b849dcf115407707ebd7a24d28ceffdd0f12d6
Pranit Khadilkar <pranit.khadilkar007@gmail.com>
2022-02-27 14:28:54 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-function App() {
-  return <h1>Welcome to React!!</h1>
+export function App() {
+  return (
+    <>
+      <h1 className="text-primary text-center">Welcome to React!!</h1>
+    </>
+  )
 }
-
-export default App
