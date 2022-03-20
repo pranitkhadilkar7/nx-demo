1c7188396d8f379e705ce97508f6bc21942f0eba
PranitK <Pranit.Khadilkar@talentica.com>
2022-03-20 21:31:04 +0530

    rfh-auth: added rfh to login form

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,31 +3,59 @@ import { useNavigate } from "react-router-dom"
 import { TextInput } from "../../common/input/TextInput"
 import { paths } from "../../routes/routes"
 import { AuthPageWrapper } from "./AuthPageWrapper"
+import { useForm } from "react-hook-form"
+import { LoginForm } from "./auth-type"
 
 export function Login() {
   const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  })
+
+  function onSubmit(data: LoginForm) {
+    console.log(data)
+  }
+
   return (
     <AuthPageWrapper
       formHeading="Hello! let's get started"
       formText="Sign in to continue."
     >
-      <form className="pt-3">
-        <TextInput type="text" placeholder="Username" />
-        <TextInput type="text" placeholder="Password" />
+      <form className="pt-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextInput
+          type="email"
+          placeholder="Email"
+          name="userEmail"
+          register={register}
+          required
+          hasError={!!errors.userEmail}
+        />
+        <TextInput
+          type="password"
+          placeholder="Password"
+          name="password"
+          register={register}
+          required
+          hasError={!!errors.password}
+        />
         <div className="mt-3">
-          <Button size="lg" className="btn btn-primary btn-block">
+          <Button size="lg" className="btn btn-primary btn-block" type="submit">
             SIGN IN
           </Button>
         </div>
         <div className="my-3 d-flex justify-content-between">
-          <div className="form-check">
+          {/* <div className="form-check">
             <label className="form-check-label text-muted">
               <input type="checkbox" className="form-check-input" />
               Keep me signed in
               <i className="input-helper"></i>
             </label>
-          </div>
-          <a href="/" className="auth-link text-black">
+          </div> */}
+          <a href="/" className="auth-link text-black mx-auto">
             Forgot Password?
           </a>
         </div>
